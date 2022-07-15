c184cb245f1c12be7217a130a3b7d104b3519df1
emeraldhycient <igwezehycient86@gmail.com>
2022-07-15 17:39:40 +0100

    [add]

--- src/components/sandbox/Hero.jsx
+++ src/components/sandbox/Hero.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import sand1 from "../../assets/images/sand1.svg";
+import sand2 from "../../assets/images/sand2.svg";
 
 function Hero() {
   return (
     <section
-      className="h-fit md:h-screen w-screen pt-12"
+      className="h-fit md:h-fit lg:h-screen w-screen pt-12 flex flex-col justify-center items-center py-20"
       style={{
         backgroundImage: `url(${sand1})`,
-        backgroundSize: "cover",
+        backgroundSize: "contain",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
       }}
     >
-      Hero
+      <h1 className="text-6xl font-extrabold md:w-[40%] mt-32 mx-auto text-transparent bg-clip-text bg-gradient-to-r from-[#222057] to-[#F8991D]">
+        We Live To Make A Difference
+      </h1>
+      <img src={sand2} alt="" className="h-80 " />
     </section>
   );
 }
--- src/pages/sandbox/Sandbox.jsx
+++ src/pages/sandbox/Sandbox.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Layout from "../../components/layout/Layout";
+import Captures from "../../components/sandbox/Captures";
 import Hero from "../../components/sandbox/Hero";
+import Projects from "../../components/sandbox/Projects";
 
 function Sandbox() {
   return (
     <Layout>
       <Header />
       <Hero />
+      <Captures />
+      <Projects />
+      <Footer />
     </Layout>
   );
 }
