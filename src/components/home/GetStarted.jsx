e1412ed5b68b456ab4a2ee7eee05c458372a4fc4
emeraldhycient <igwezehycient86@gmail.com>
2022-07-28 14:26:44 +0100

    [update] changed the getstarted button link

--- src/components/home/GetStarted.jsx
+++ src/components/home/GetStarted.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function GetStarted() {
   return (
     <button className="bg-amber-500 text-[12px] text-white px-8 py-2 rounded w-[80%] mx-auto lg:mx-0 lg:w-fit flex justify-center items-center">
-      <Link to="/create-account" className="flex items-center">
+      <Link to="/advisor" className="flex items-center">
         Get Started
       </Link>
     </button>
