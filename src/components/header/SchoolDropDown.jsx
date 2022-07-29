653a037b7285e011ae89be0bfe4734ca7734bb77
emeraldhycient <igwezehycient86@gmail.com>
2022-07-29 16:56:46 +0100

    [update]

--- src/components/header/SchoolDropDown.jsx
+++ src/components/header/SchoolDropDown.jsx
@@ -2,54 +2,50 @@ import React from "react";
 
 function SchoolDropDown() {
   return (
-    <div className="">
-      <div className="grid grid-cols-3 p-8">
-        <div className="flex justify-center items-center flex-col">
-          <h1 className="text-[16px] font-bold  font-gilroy  w-full  text-[#222057] mb-2 md:mb-4 ">
-            <a href="/schools/product">Product School</a>
-          </h1>
-          <ul className="w-full">
-            <li className="font-gilroyregular text-[14px] my-1">
-              <a href="/schools/product/product-design">Product Design</a>
-            </li>
-            <li className="font-gilroyregular text-[14px] my-1">
-              <a href="/schools/product/product-management">
-                Product Management
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="flex justify-center items-center flex-col">
-          <h1 className="text-[16px] font-bold  font-gilroy  w-full  text-[#222057] mb-2 md:mb-4 ">
-            <a href="/schools/data">Data School</a>
-          </h1>
-          <ul className="w-full">
-            <li className="font-gilroyregular text-[14px] my-1">
-              <a href="/schools/data/data-analysis">Data Analytics</a>
-            </li>
-            <li className="font-gilroyregular text-[14px] my-1">
-              <a href="/schools/data/machine-learning">Machine Learning</a>
-            </li>
-          </ul>
-        </div>
-        <div className="flex justify-center items-center flex-col">
-          <h1 className="text-[16px] font-bold  font-gilroy  w-full  text-[#222057] mb-2 md:mb-4 ">
-            <a href="/schools/programming">Programming School</a>
-          </h1>
-          <ul className="w-full">
-            <li className="font-gilroyregular text-[14px] my-1">
-              <a href="/schools/programming/frontend-development">
-                Front-End Development
-              </a>
-            </li>
+    <div className="grid grid-cols-2 md:grid-cols-3 p-0 m-0 md:p-8">
+      <div className="flex justify-center items-center flex-col">
+        <h1 className="text-[16px] font-bold  font-gilroy  w-full  text-[#222057] mb-2 md:mb-4 ">
+          <a href="/schools/product">Product School</a>
+        </h1>
+        <ul className="w-full">
+          <li className="font-gilroyregular text-[14px] my-1">
+            <a href="/schools/product/product-design">Product Design</a>
+          </li>
+          <li className="font-gilroyregular text-[14px] my-1">
+            <a href="/schools/product/product-management">Product Management</a>
+          </li>
+        </ul>
+      </div>
+      <div className="flex justify-center items-center flex-col ">
+        <h1 className="text-[16px] font-bold  font-gilroy  w-full  text-[#222057] mb-2 md:mb-4 ">
+          <a href="/schools/data">Data School</a>
+        </h1>
+        <ul className="w-full">
+          <li className="font-gilroyregular text-[14px] my-1">
+            <a href="/schools/data/data-analysis">Data Analytics</a>
+          </li>
+          <li className="font-gilroyregular text-[14px] my-1">
+            <a href="/schools/data/machine-learning">Machine Learning</a>
+          </li>
+        </ul>
+      </div>
+      <div className="flex justify-center items-center flex-col mt-5 md:mt-0">
+        <h1 className="text-[16px] font-bold  font-gilroy  w-full  text-[#222057] mb-2 md:mb-4 ">
+          <a href="/schools/programming">Programming School</a>
+        </h1>
+        <ul className="w-full">
+          <li className="font-gilroyregular text-[14px] my-1">
+            <a href="/schools/programming/frontend-development">
+              Front-End Development
+            </a>
+          </li>
 
-            <li className="font-gilroyregular text-[14px] my-1">
-              <a href="/schools/programming/fullstack-development">
-                fullstack Development
-              </a>
-            </li>
-          </ul>
-        </div>
+          <li className="font-gilroyregular text-[14px] my-1">
+            <a href="/schools/programming/fullstack-development">
+              fullstack Development
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
