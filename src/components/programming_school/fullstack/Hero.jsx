72202cf9a21d5b890ee4920a6b040d79c2db30f9
emeraldhycient <igwezehycient86@gmail.com>
2022-07-29 14:56:18 +0100

    [add]

--- src/components/programming_school/fullstack/Hero.jsx
+++ src/components/programming_school/fullstack/Hero.jsx
@@ -71,7 +71,7 @@ function Hero() {
         </p>
         <h4 className="mb-4">Duration: 28 Weeks | Beginner Level</h4>
         <div className="lg:flex flex-col md:flex-row justify-between  items-center">
-          <ApplyNow course="front end" reference="62bb0bc968666" />
+          <ApplyNow course="full stack" reference="62bb0bc968666" />
 
           <button className="bg-white border-[.001rem] border-slate-500 text-amber-500 text-white px-8 py-2 text-[12px] rounded mt-3 lg:mt-0 lg:ml-3 w-full md:w-[100%] mx-auto lg:w-fit flex justify-center items-center">
             <Link to="/advisor" className="flex items-center text-amber-500">
