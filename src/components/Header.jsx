0ed8adf95cbfb5caecb7a8998301090e712a062e
emeraldhycient <igwezehycient86@gmail.com>
2022-07-29 16:50:34 +0100

    [update] sticky top with increased zindex

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,50 +9,52 @@ import MoreDropDown from "./header/MoreDropDown";
 
 function Header() {
   return (
-    <Navbar fluid={true} rounded={true}>
-      <Navbar.Brand href="https://flowbite.com/">
-        <img
-          src={logo}
-          className="mr-3 ml-6 h-6 sm:h-9"
-          alt="pluralcode Logo"
-        />
-      </Navbar.Brand>
-      <div className="flex md:order-2">
-        <GetStarted />
-        <Navbar.Toggle />
-      </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="/">Home</Navbar.Link>
-        <Navbar.Link>
-          <Dropdown arrowIcon={true} inline={true} label={"Schools"}>
-            <Dropdown.Item>
-              <SchoolDropDown />
-            </Dropdown.Item>
-          </Dropdown>
-        </Navbar.Link>
-        <Navbar.Link>
-          <Dropdown arrowIcon={true} inline={true} label={"Platform"}>
-            <Dropdown.Item>
-              <PlatformDropDown />
-            </Dropdown.Item>
-          </Dropdown>
-        </Navbar.Link>
-        <Navbar.Link>
-          <Dropdown arrowIcon={true} inline={true} label={"Enterprise"}>
-            <Dropdown.Item>
-              <EnterpriseDropDown />
-            </Dropdown.Item>
-          </Dropdown>
-        </Navbar.Link>
-        <Navbar.Link>
-          <Dropdown arrowIcon={true} inline={true} label={"More"}>
-            <Dropdown.Item>
-              <MoreDropDown />
-            </Dropdown.Item>
-          </Dropdown>
-        </Navbar.Link>{" "}
-      </Navbar.Collapse>
-    </Navbar>
+    <div className="sticky top-0 backdrop-grayscale z-40">
+      <Navbar fluid={true} rounded={true}>
+        <Navbar.Brand href="https://flowbite.com/">
+          <img
+            src={logo}
+            className="mr-3 ml-6 h-6 sm:h-9"
+            alt="pluralcode Logo"
+          />
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          <GetStarted />
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <Navbar.Link href="/">Home</Navbar.Link>
+          <Navbar.Link>
+            <Dropdown arrowIcon={true} inline={true} label={"Schools"}>
+              <Dropdown.Item>
+                <SchoolDropDown />
+              </Dropdown.Item>
+            </Dropdown>
+          </Navbar.Link>
+          <Navbar.Link>
+            <Dropdown arrowIcon={true} inline={true} label={"Platform"}>
+              <Dropdown.Item>
+                <PlatformDropDown />
+              </Dropdown.Item>
+            </Dropdown>
+          </Navbar.Link>
+          <Navbar.Link>
+            <Dropdown arrowIcon={true} inline={true} label={"Enterprise"}>
+              <Dropdown.Item>
+                <EnterpriseDropDown />
+              </Dropdown.Item>
+            </Dropdown>
+          </Navbar.Link>
+          <Navbar.Link>
+            <Dropdown arrowIcon={true} inline={true} label={"More"}>
+              <Dropdown.Item>
+                <MoreDropDown />
+              </Dropdown.Item>
+            </Dropdown>
+          </Navbar.Link>{" "}
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   );
 }
 
