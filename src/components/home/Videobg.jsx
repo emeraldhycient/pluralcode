71212afa8a6e9485f4ac91d0ee1b1e487185aafc
emeraldhycient <igwezehycient86@gmail.com>
2022-07-30 20:54:50 +0100

    [update]

--- src/components/home/Videobg.jsx
+++ src/components/home/Videobg.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./modal.css";
 import videobg from "../../assets/images/videobg.png";
+import play from "../../assets/images/play.png";
 import ModalVideo from "./ModalVideo";
 
 function Videobg() {
@@ -24,19 +25,8 @@ function Videobg() {
         />
       )}
       {!isOpen && (
-        <button className="" id="container15" onClick={(e) => setOpen(!isOpen)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-12 w-12 text-[#222057]"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-              clipRule="evenodd"
-            />
-          </svg>
+        <button className="" onClick={(e) => setOpen(!isOpen)}>
+          <img src={play} alt="play" />
         </button>
       )}
     </section>
