f1171020a80c375f00f1c3b4cb283b7b6ddc874a
emeraldhycient <igwezehycient86@gmail.com>
2022-10-07 14:19:57 +0100

    [update] added dropdown

--- src/components/layout/sidebarContent.jsx
+++ src/components/layout/sidebarContent.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from "react-icons/md"
 
 function SidebarContent({ item }) {
 
     const location = useLocation();
 
+    const [isdropdownactive, setisdropdownactive] = useState(false)
+
 
     return (
-        <li className={location.pathname === item.link ? "bg-[#F5F6FA] w-full py-3 flex justify-center items-center text-[#F8991D] my-6 border-l-[0.3rem] border-amber-500" : "py-3 flex justify-center items-center text-[#232323] my-6"} >
-            <a href={item.link} className="flex items-center  w-[70%] text-left" >
-                <span className='mr-2'>
-                    <img src={item.icon} />
-                </span>
-                <h4>{item.title}</h4>
-            </a>
-        </li>
+        <>
+            <li className={location.pathname === item.link ? "bg-[#F5F6FA] w-full py-3 flex justify-center items-center text-[#F8991D] my-6 border-l-[0.3rem] border-amber-500" : "py-3 flex justify-center items-center text-[#232323] my-6"} onClick={() => {
+                setisdropdownactive(!isdropdownactive)
+            }} >
+                {item.link ?
+                    <a href={item.link} className="flex items-center  w-[70%] text-left" >
+                        <span className='mr-2'>
+                            <img src={item.icon} />
+                        </span>
+                        <h4>{item.title}</h4>
+                        {item.children ? isdropdownactive ? <MdOutlineKeyboardArrowDown /> : <MdOutlineKeyboardArrowUp /> : null}
+                    </a> : <a className="flex items-center  w-[70%] text-left" >
+                        <span className='mr-2'>
+                            <img src={item.icon} />
+                        </span>
+                        <h4>{item.title}</h4>
+                        {item.children ? isdropdownactive ? <MdOutlineKeyboardArrowDown /> : <MdOutlineKeyboardArrowUp /> : null}
+                    </a>}
+            </li>
+            {
+                isdropdownactive ?
+                    item.children ?
+                        item.children.map((data) => (
+                            <li className={location.pathname === data.link ? "bg-[#F5F6FA] w-full py-3 flex justify-center items-center text-[#F8991D] my-3 border-l-[0.3rem] border-amber-500" : "py-3 flex justify-center items-center text-[#232323] my-3 ml-6"} >
+                                <a href={data.link} className="flex items-center  w-[70%] text-left" >
+                                    <span className='mr-2'>
+                                        {data.icon ? <img src={item.icon} /> : null}
+                                    </span>
+                                    <h4 className='text-[16px]'>{data.title}</h4>
+                                </a>
+                            </li>
+                        ))
+                        : null : null
+            }
+
+        </>
     )
 }
 
--- src/data/sidebardata.js
+++ src/data/sidebardata.js
@@ -4,6 +4,7 @@ import { IoMdNotificationsOutline } from "react-icons/io"
 import { FaUserGraduate } from "react-icons/fa"
 
 import dashboard_icon from "../assets/dashboard_icon.svg"
+import courses_icon from "../assets/courses_icon.svg"
 import exclusive_icon from "../assets/exclusive_icon.svg"
 import mycourses_icon from "../assets/mycourses_icon.svg"
 import advsior_icon from "../assets/advsior_icon.svg"
@@ -11,6 +12,7 @@ import pluralcodetv_icon from "../assets/pluralcodetv_icon.svg"
 import notification_icon from "../assets/notification_icon.svg"
 import profile_icon from "../assets/profile_icon.svg"
 import logout_icon from "../assets/logout_icon.svg"
+import { Children } from "react"
 
 const sidebardata = [
     {
@@ -18,6 +20,32 @@ const sidebardata = [
         icon: dashboard_icon,
         link: "/student/dashboard"
 
+    },
+    {
+        title: "Courses",
+        icon: courses_icon,
+        children: [
+            {
+                title: "Data school",
+                link: "/student/dashboard/data"
+
+            },
+            {
+                title: "Product School",
+                link: "/student/dashboard/product"
+
+            },
+            {
+                title: "Programming School",
+                link: "/student/dashboard/programming"
+
+            },
+            {
+                title: "Cloud school",
+                link: "/student/dashboard/cloud"
+
+            },
+        ]
     },
     {
         title: "Exclusive",
