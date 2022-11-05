dfde6167fbe3d4781732a6e016db6ec9c03bc9a1
emeraldhycient <igwezehycient86@gmail.com>
2022-11-05 08:05:54 +0100

    [update] working on the exclusive form

--- src/pages/exclusive/Exclusive.jsx
+++ src/pages/exclusive/Exclusive.jsx
@@ -5,12 +5,14 @@ import VideoContainer from '../../components/exclusive/VideoContainer'
 import data_analytics from "../../assets/data_analytics.svg"
 import Faq from '../../components/exclusive/Faq'
 
+import ExclusiveCourses from "./data"
+
 
 function Exclusive() {
     return (
         <DashboardLayout>
             <div className='h-fit w-full  rounded-[8px] bg-transparent md:bg-white p-5 mb-4'>
-                <VideoContainer />
+                <VideoContainer video_url={"https://res.cloudinary.com/gofitish/video/upload/v1663331212/videos/WhatsApp_Video_2022-09-16_at_1.17.41_PM_knyugc.mp4"} />
                 <h3 className='text-[26px] md:text-[32px] text-[#222057] mb-5'>PluralCode Exclusive</h3>
                 <p className='text-[16px] mb-2 text-[#323232]'>Data Analytics program provides a range of ​data and analytics​ training designed to help ​individuals as well as ​organisations​, ​enable new and existing internal resources through the use of key data science tools and platforms.
                 </p>
@@ -21,11 +23,11 @@ function Exclusive() {
             <div className='h-fit w-full  rounded-[8px] bg-white p-5 mt-8 md:mt-20'>
                 <h3 className='text-[26px] md:text-[32px] text-[#222057] mb-5'>Our Exclusive Program Courses</h3>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-                    <ExclusiveCourseCard />
-                    <ExclusiveCourseCard />
-                    <ExclusiveCourseCard />
-                    <ExclusiveCourseCard />
-                    <ExclusiveCourseCard />
+                    {
+                        ExclusiveCourses.map((item) => (
+                            <ExclusiveCourseCard item={item} key={item.id} />
+                        ))
+                    }
                 </div>
             </div>
             <div className='h-fit w-full  rounded-[8px] bg-white p-5 mt-16 md:mt-20'>
@@ -36,40 +38,43 @@ function Exclusive() {
     )
 }
 
-const ExclusiveCourseCard = () => (
-    <div
-        className="bg-[#F5F6FA] h-fit  my-4 rounded-tl-3xl rounded-br-3xl py-6 px-3 mb-4"
-        data-aos="fade-right"
-        data-aos-duration="2000"
-    >  <div >
-            <img
-                src={data_analytics}
-                alt="product"
-                className="w-[90%]  rounded-tl-3xl rounded-br-3xl"
-                data-aos="fade-right"
-                data-aos-duration="2000"
-            />
-        </div>
-        <div className="pb-4 flex flex-col justify-center mt-4">
-            <h1
-                className="text-[25px] text-[#222057] font-medium mb-4"
-                data-aos="fade-right"
-                data-aos-duration="2000"
-            >
-                Azure Cloud Computing
-            </h1>
-            <p
-                className="font-gilroyregular text-sm  lg:text-left  font-extralight text-[#323232] mb-2"
-                data-aos="fade-right"
-                data-aos-duration="2000"
-            >
-                Learn to use popular Cloud platforms, understand Cloud computing concepts and technologies apply virtualization and its use in Infrastructure development.
-            </p>
-            <div className="w-fit mt-3">
-                <CustomButton>Apply Now</CustomButton>
+const ExclusiveCourseCard = (item) => {
+    console.log(item)
+    return (
+        <div
+            className="bg-[#F5F6FA] h-fit  my-4 rounded-tl-3xl rounded-br-3xl py-6 px-3 mb-4"
+            data-aos="fade-right"
+            data-aos-duration="2000"
+        >  <div >
+                <img
+                    src={item.item.image}
+                    alt="product"
+                    className="w-[90%]  rounded-tl-3xl rounded-br-3xl"
+                    data-aos="fade-right"
+                    data-aos-duration="2000"
+                />
+            </div>
+            <div className="pb-4 flex flex-col justify-center mt-4">
+                <h1
+                    className="text-[25px] text-[#222057] font-medium mb-4"
+                    data-aos="fade-right"
+                    data-aos-duration="2000"
+                >
+                    {item.item.title}
+                </h1>
+                <p
+                    className="font-gilroyregular text-sm  lg:text-left  font-extralight text-[#323232] mb-2"
+                    data-aos="fade-right"
+                    data-aos-duration="2000"
+                >
+                    {item.item.description}
+                </p>
+                <div className="w-fit mt-3">
+                    <CustomButton>Apply Now</CustomButton>
+                </div>
             </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default Exclusive
---
+++ src/pages/exclusive/data.js
@@ -0,0 +1,54 @@
+
+const ExclusiveCourses = [
+    {
+        id: 1,
+        title: "Product Design",
+        description: "Learn Ui/Ux from scratch without writing a single code. Master how to design high fidelity interface with FIGMA, design journey mapping and empathize with users.",
+        image: "https://i.im.ge/2022/11/05/2vG0vh.health-tracking-glassmorphic-design-neumorphic-elements-kit-mobile-app-ui-ux-gui-screens-set-9209-5179-1.png"
+    },
+    {
+        id: 2,
+        title: "Product Management",
+        description: "Become a professional product manager in 8 weeks. Learn how to gather users data, identify their needs and how it benefits them.",
+        image: "https://i.im.ge/2022/11/05/2vGtep.Group-50.png"
+    },
+    {
+        id: 3,
+        title: "Data Analysis",
+        description: "Learn to build Financial models, forecast performances and perform Valuations for companies. ",
+        image: "https://i.im.ge/2022/11/05/2vGCE1.Group-50-1.png"
+    },
+    {
+        id: 4,
+        title: "Data Science",
+        description: "Analyze and visualize data using Machine learning models. This course dives into the basics of machine learning using an approachable, and well-known programming language, Python.",
+        image: "https://i.im.ge/2022/11/05/2vGEvf.Group-50-2.png"
+    },
+    {
+        id: 5,
+        title: "Front-end Web Development",
+        description: "Learn how to create international standard website and apps using the most recent front End frameworks of the day.",
+        image: "https://i.im.ge/2022/11/05/2vGYNr.Group-50-3.png"
+    },
+    {
+        id: 6,
+        title: "Full-Stack Web Development",
+        description: "Learn how to create international standard website and apps using the most recent front End frameworks of the day.",
+        image: "https://i.im.ge/2022/11/05/2vGbVW.Group-50-4.png"
+    },
+    {
+        id: 7,
+        title: "Azure Cloud Computing",
+        description: "Learn to use popular Cloud platforms, understand Cloud computing concepts and technologies apply virtualization and its use in Infrastructure development.",
+        image: "https://i.im.ge/2022/11/05/2vGRgc.Group-50-5.png"
+    },
+    {
+        id: 8,
+        title: "DevOps Engineering",
+        description: "Become an expert in the principles of continuous development and deployment, automation of configuration management, inter-team collaboration and IT service agility using DevOps tools such as Git, Docker, Jenkins and more",
+        image: "https://i.im.ge/2022/11/05/2vGZML.Group-50-6.png"
+    }
+]
+
+
+export default ExclusiveCourses;
