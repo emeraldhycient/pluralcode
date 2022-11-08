100f8f12c5f65cc6ebe441b1e695f5b6e56ce42a
emeraldhycient <igwezehycient86@gmail.com>
2022-11-08 13:54:53 +0100

    [update] integrated api into notification

--- src/pages/notifications/Notifications.jsx
+++ src/pages/notifications/Notifications.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import DashboardLayout from '../../components/layout/DashboardLayout'
+import axiosClient from '../../services/apiClient'
 
 function Notifications() {
+
+    const [data, setdata] = useState([])
+    useEffect(() => {
+        const getNotifi = async () => {
+            try {
+                const res = await axiosClient.get("get_notification_list")
+                console.log(res);
+                setdata(res.data)
+            } catch (error) {
+                console.error(error.response);
+            }
+        }
+        getNotifi();
+    }, [])
+
+
     return (
         <DashboardLayout> <div className="bg-white h-fit rounded-[8px] p-5 md:p-10">
             <h4 className='text-[28px] text-[#222057]'>Notifications</h4>
             <div className=" mt-8">
-                <NotificationCard />
-                <NotificationCard />
-                <NotificationCard />
+                {
+                    data.length > 0 ?
+                        data.map((item) => (
+                            <NotificationCard item={item} />
+
+                        )) : <NotificationCard item={{ body: "No Notifications", date: "" }} />
+                }
+
             </div>
         </div>
         </DashboardLayout>
     )
 }
 
-const NotificationCard = () => (
+const NotificationCard = ({ item }) => (
     <div className="flex items-center justify-between mb-4">
         <div className="flex items-center">
             <div className="bg-gray-200 h-3 md:h-4 w-3 md:w-4 rounded-full mr-2"></div>
-            <p className='text-sm text-[#232323]'>Your complete payment for product design course was successful</p>
+            <p className='text-sm text-[#232323]'>{item.body}</p>
         </div>
-        <p className="text-xs">2:34pm</p>
+        <p className="text-xs">{item.date}</p>
     </div>
 )
 
