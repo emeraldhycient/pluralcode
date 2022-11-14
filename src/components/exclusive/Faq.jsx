405f878ea3f8fb5112562606e175452146366c4b
emeraldhycient <igwezehycient86@gmail.com>
2022-11-14 14:56:14 +0100

    [update] faq content

--- src/components/exclusive/Faq.jsx
+++ src/components/exclusive/Faq.jsx
@@ -6,77 +6,68 @@ function Faq() {
         <Accordion >
             <Accordion.Panel>
                 <Accordion.Title>
-                    What is Flowbite?
-                </Accordion.Title>
+                    Are trainings online?</Accordion.Title>
                 <Accordion.Content>
-                    <p className="mb-2 text-gray-500 dark:text-gray-400">
-                        Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.
-                    </p>
+
                     <p className="text-gray-500 dark:text-gray-400">
-                        Check out this guide to learn how to{' '}
-                        <a
-                            href="https://flowbite.com/docs/getting-started/introduction/"
-                            className="text-blue-600 hover:underline dark:text-blue-500"
-                        >
-                            get started
-                        </a>
-                        {' '}and start developing websites even faster with components on top of Tailwind CSS.
+                        Trainings are strictly virtual.
                     </p>
                 </Accordion.Content>
             </Accordion.Panel>
             <Accordion.Panel>
                 <Accordion.Title>
-                    Is there a Figma file available?
+                    Can I be mentored in my house ?
                 </Accordion.Title>
                 <Accordion.Content>
                     <p className="mb-2 text-gray-500 dark:text-gray-400">
-                        Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.
+                        Trainings are generally taken virtual, however physical home mentorship is open based on negotiation  is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.
                     </p>
-                    <p className="text-gray-500 dark:text-gray-400">
-                        Check out the{' '}
-                        <a
-                            href="https://flowbite.com/figma/"
-                            className="text-blue-600 hover:underline dark:text-blue-500"
-                        >
-                            Figma design system
-                        </a>
-                        {' '}based on the utility classes from Tailwind CSS and components from Flowbite.
+
+                </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+                <Accordion.Title>
+                    Can I pay in installments?
+                </Accordion.Title>
+                <Accordion.Content>
+                    <p className="mb-2 text-gray-500 dark:text-gray-400">
+                        Yes payments on installments are allowed.
                     </p>
                 </Accordion.Content>
             </Accordion.Panel>
             <Accordion.Panel>
                 <Accordion.Title>
-                    What are the differences between Flowbite and Tailwind UI?
+                    How long are programs under the exclusive option?
                 </Accordion.Title>
                 <Accordion.Content>
                     <p className="mb-2 text-gray-500 dark:text-gray-400">
-                        The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.
-                    </p>
+                        Courses that normally take 6 months to learn normally will take only 3-4 months under the exclusive program                     </p>
+                </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+                <Accordion.Title>
+                    Do you have certified mentors?
+                </Accordion.Title>
+                <Accordion.Content>
                     <p className="mb-2 text-gray-500 dark:text-gray-400">
-                        However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.
+                        Our mentors are industry professionals who are tested and trusted
                     </p>
+                </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+                <Accordion.Title>
+                    How many courses are available under the Exclusive program?                </Accordion.Title>
+                <Accordion.Content>
                     <p className="mb-2 text-gray-500 dark:text-gray-400">
-                        Learn more about these technologies:
-                    </p>
-                    <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-                        <li>
-                            <a
-                                href="https://flowbite.com/pro/"
-                                className="text-blue-600 hover:underline dark:text-blue-500"
-                            >
-                                Flowbite Pro
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="https://tailwindui.com/"
-                                rel="nofollow"
-                                className="text-blue-600 hover:underline dark:text-blue-500"
-                            >
-                                Tailwind UI
-                            </a>
-                        </li>
-                    </ul>
+                        All our courses are available under the exclusive program.                    </p>
+                </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+                <Accordion.Title>
+                    Can I learn more than one course?                </Accordion.Title>
+                <Accordion.Content>
+                    <p className="mb-2 text-gray-500 dark:text-gray-400">
+                        Students can learn more than one course but both courses have to be under the same field. E.g Product design /management                    </p>
                 </Accordion.Content>
             </Accordion.Panel>
         </Accordion>
